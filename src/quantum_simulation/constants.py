fd2350c711655d766e4bae1ef5365d7592c754cf
Lukas Reithmeier <lukas.reithmeier@fh-hagenberg.at>
2022-04-10 22:19:49 +0200

    changed transformations to constants

---
+++ src/quantum_simulation/constants.py
@@ -0,0 +1,41 @@
+import math
+from enum import Enum
+
+import numpy
+import numpy as np
+
+
+class Constants:
+    PAULI_I = np.array([[1, 0],
+                        [0, 1]])
+
+    PAULI_X = np.array([[0, 1],
+                        [1, 0]])
+
+    PAULI_Y = np.array([[0, 1],
+                        [-1, 0]])
+
+    PAULI_Z = np.array([[1, 0],
+                        [0, -1]])
+
+    HADAMARD = (1 / math.sqrt(2)) * np.array([[1, 1],
+                                              [1, -1]])
+
+    BELL = np.array([1 / math.sqrt(2), 0, 0, 1 / math.sqrt(2)])
+
+    CNOT = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 1, 0]
+    ])
+
+
+def main():
+    """
+    main
+    """
+
+
+if __name__ == "__main__":
+    main()
